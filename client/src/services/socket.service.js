522a991881aec0471cd9671923ce82e01771239c
Flintoffjerisildas <145636771+Flintoffjerisildas@users.noreply.github.com>
2026-02-10 11:57:53 +0530

    get task socket.service updated io to the api

--- client/src/services/socket.service.js
+++ client/src/services/socket.service.js
@@ -3,79 +3,110 @@ import io from 'socket.io-client';
 const SOCKET_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
 
 class SocketService {
-    socket;
+    socket = null;
+    pendingEmits = [];
+    listeners = new Map();
 
     connect(options = {}) {
+        if (this.socket && this.socket.connected) return this.socket;
+
         this.socket = io(SOCKET_URL, {
             ...options,
-            transports: ['websocket', 'polling'], // Try websocket first
+            transports: ['websocket', 'polling'],
             withCredentials: true,
             reconnectionAttempts: 5,
         });
 
         this.socket.on('connect', () => {
             console.log('Socket Connected:', this.socket.id);
+            this._processPendingActions();
         });
 
         this.socket.on('connect_error', (err) => {
             console.error('Socket Connection Error:', err);
         });
 
+        // Re-attach listeners if they exist
+        this.listeners.forEach((callback, event) => {
+            if (!this.socket.hasListeners(event)) {
+                this.socket.on(event, callback);
+            }
+        });
+
         return this.socket;
+    }
+
+    _processPendingActions() {
+        if (!this.socket) return;
+
+        // Process pending emits
+        while (this.pendingEmits.length > 0) {
+            const { event, data } = this.pendingEmits.shift();
+            this.socket.emit(event, data);
+        }
     }
 
     disconnect() {
         if (this.socket) {
             this.socket.disconnect();
+            this.socket = null;
+        }
+        this.pendingEmits = [];
+        this.listeners.clear();
+    }
+
+    _emit(event, data) {
+        if (this.socket && this.socket.connected) {
+            this.socket.emit(event, data);
+        } else {
+            this.pendingEmits.push({ event, data });
+        }
+    }
+
+    _on(event, callback) {
+        this.listeners.set(event, callback);
+        if (this.socket) {
+            this.socket.on(event, callback);
+        }
+    }
+
+    _off(event) {
+        this.listeners.delete(event);
+        if (this.socket) {
+            this.socket.off(event);
         }
     }
 
     joinProject(projectId) {
-        if (this.socket) {
-            this.socket.emit('join_project', projectId);
-        }
+        this._emit('join_project', projectId);
     }
 
     leaveProject(projectId) {
-        if (this.socket) {
-            this.socket.emit('leave_project', projectId);
-        }
+        this._emit('leave_project', projectId);
     }
 
     joinWorkspace(workspaceId) {
-        if (this.socket) {
-            this.socket.emit('join_workspace', workspaceId);
-        }
+        this._emit('join_workspace', workspaceId);
     }
 
     leaveWorkspace(workspaceId) {
-        if (this.socket) {
-            this.socket.emit('leave_workspace', workspaceId);
-        }
+        this._emit('leave_workspace', workspaceId);
     }
 
     sendMessage(messageData) {
-        if (this.socket) {
-            this.socket.emit('send_message', messageData);
-        }
+        this._emit('send_message', messageData);
     }
 
     onReceiveMessage(callback) {
-        if (this.socket) {
-            this.socket.on('receive_message', callback);
-        }
+        this._on('receive_message', callback);
     }
 
     onNotification(callback) {
-        if (this.socket) {
-            this.socket.on('new_notification', callback);
-        }
+        this._on('new_notification', callback);
     }
 
     offReceiveMessage() {
-        if (this.socket) {
-            this.socket.off('receive_message');
-        }
+        this._off('receive_message');
     }
 }
 
